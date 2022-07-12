de39b34f6bc75d6711bba55f34ca706ea1d5b035
an dong woo <akskwha@naver.com>
2022-07-12 18:38:33 +0900

    0712정리 실습문제

---
+++ 문제8/곱 구하기.py
@@ -0,0 +1,12 @@
+
+
+n = int (input())
+
+sum = 1
+
+i = 1
+
+while i <= n:
+    sum *= i
+    i += 1
+print(sum)
---
+++ 문제8/길이  구하기.py
@@ -0,0 +1,7 @@
+word = 'happy!'
+a = 0
+
+while a < len(word):
+    a = a+1
+if a >=len(word):
+    print(a)
