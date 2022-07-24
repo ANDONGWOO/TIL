750892d58b0a0456f2bf0393dcb065b503a44e5f
an dong woo <akskwha@naver.com>
2022-07-25 02:14:12 +0900

    문제풀이

--- 실습/문제08.두번째로 큰 수 구하기.py
+++ 실습/문제08.두번째로 큰 수 구하기.py
@@ -2,14 +2,10 @@
 #max()  함수 사용금지
 # 중복제거
 # 정렬 적용?
-#
-numbers = [0, 20, 100, 50, -60, 50, 100]
 
-new_list = []
-a = new_list[0]
-for v in numbers:
-    if v not in new_list:
-        new_list.append(v)
-    
-         
-print(new_list)
+numbers =  [0, 20, 100]
+
+a= set(numbers)
+b= list(a)
+b.sort()
+print (b[-2])
