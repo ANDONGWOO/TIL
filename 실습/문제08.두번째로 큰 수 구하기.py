42e53bdc83d01bf8f9f54c8f39218c6af9fa46e8
an dong woo <akskwha@naver.com>
2022-07-24 23:22:27 +0900

    문제 풀이

--- 실습/문제08.두번째로 큰 수 구하기.py
+++ 실습/문제08.두번째로 큰 수 구하기.py
@@ -1,7 +1,8 @@
 #주어진 리스트 numbers에서 두번째로 큰 수를 구하여 출력하시오
 #max()  함수 사용금지
-
-
+# 중복제거
+# 정렬 적용?
+#
 numbers = [0, 20, 100, 50, -60, 50, 100]
 
 new_list = []
@@ -10,7 +11,5 @@ for v in numbers:
     if v not in new_list:
         new_list.append(v)
     
-for i in new_list:
-    if a < i:
-       a = i          
-print(a)
+         
+print(new_list)
