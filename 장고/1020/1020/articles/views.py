3f67d03f7c25eccc873b4090bbcbc6747a376c81
an dong woo <akskwha@naver.com>
2022-10-20 17:07:29 +0900

    병합

--- 장고/1020/1020/articles/views.py
+++ 장고/1020/1020/articles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
+from django.shortcuts import render,redirect
+from .forms import ArticleForm,CommentForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def create(request):
+    if request.method == "POST":
+        article_Form=ArticleForm(request.POST, request.FILES)
+        if article_Form.is_valid():
+            article_Form.save()
+            return redirect('articles:index')
+    else:
+        article_Form=ArticleForm()
+    context={
+        'article_Form':article_Form
+    }
+    return render(request, 'articles/create.html',context)
--- 장고/1020/1020/day16/settings.py
+++ 장고/1020/1020/day16/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'imagekit',
     "django_bootstrap5",
     'accounts',
     'articles',
@@ -106,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'ASIA/SEOUL'
 
 USE_I18N = True
 
@@ -122,7 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
