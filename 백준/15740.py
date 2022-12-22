fde78895aee873381cfe506600fc71c679938e38
an dong woo <akskwha@naver.com>
2022-12-22 23:27:21 +0900

    221222문제풀이

---
+++ 백준/15740.py
@@ -0,0 +1,6 @@
+import sys
+
+sys.stdin = open("15740.txt", "r")
+
+A,B=map(int,input().split())
+print(A+B)
---
+++ 백준/26711.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.stdin = open("26711.txt", "r")
+
+a=int(input())
+b=int(input())
+print(a+b)
