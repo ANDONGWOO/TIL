7f0142cf776480abca81ee1facd3502d4c7357ab
an dong woo <akskwha@naver.com>
2023-01-16 22:48:30 +0900

    230116문제풀이

---
+++ 백준/1212.py
@@ -0,0 +1,7 @@
+import sys
+
+sys.stdin = open("1212.txt", "r")
+
+print(bin(int(input(),8))[2:])
+
+#bin 2진법
---
+++ 백준/1373.py
@@ -0,0 +1,6 @@
+import sys
+
+sys.stdin = open("1373.txt", "r")
+
+print(oct(int(input(),2))[2:])
+#oct 8진법
