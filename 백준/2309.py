3560613019791a6089382790952357da272f98cb
an dong woo <akskwha@naver.com>
2023-01-31 23:13:38 +0900

    230131문제풀이

---
+++ 백준/2309.py
@@ -0,0 +1,10 @@
+import sys
+import itertools
+sys.stdin = open("2309.txt", "r")
+
+t=[int(input()) for _ in range(9)]
+for i in itertools.combinations(t,7):#itertools.combinations 조합 7개의 조합 중복x
+    if sum(i)==100:
+        for k in sorted(i):
+            print(k)
+        break
