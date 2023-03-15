83c25d55b59aca0e049df16a45928d6a2cfd40a4
an dong woo <akskwha@naver.com>
2023-03-15 19:59:10 +0900

    230315문제풀이

---
+++ 백준/java/BOJ_1001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * BOJ_1001
+ */
+public class BOJ_1001 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a-b);
+
+        in.close();
+    }
+}
