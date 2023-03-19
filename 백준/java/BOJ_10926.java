5e9799cddcadb06ef5897ed7f80f764ecc1301dc
an dong woo <akskwha@naver.com>
2023-03-19 21:33:22 +0900

    230319문제풀이

---
+++ 백준/java/BOJ_10926.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class BOJ_10926 {
+    
+    public static void main(String[] args) {
+        String a = "??!";
+        Scanner in = new Scanner(System.in);
+
+        String b= in.nextLine();//입력 값
+        System.out.println(b+a);
+        in.close();
+    }
+}
