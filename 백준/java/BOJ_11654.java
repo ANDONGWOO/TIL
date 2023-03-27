59f088bdb5da77bf12129285a21584ef7716498c
an dong woo <akskwha@naver.com>
2023-03-27 23:26:09 +0900

    230327문제풀이

---
+++ 백준/java/BOJ_11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BOJ_11654 {
+
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int q =in.next().charAt(0);
+        System.out.print(q);
+        in.close();
+    }
+}
