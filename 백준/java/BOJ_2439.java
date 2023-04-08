65e6fd2832104060775acb495265b9b4f41ff04b
an dong woo <akskwha@naver.com>
2023-04-08 21:07:37 +0900

    230408문제풀이

---
+++ 백준/java/BOJ_2439.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BOJ_2439 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s=in.nextInt();
+        for(int i=1; i<=s; i++){
+            for (int j = 1; j<=s-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
