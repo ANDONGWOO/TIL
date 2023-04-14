2f4929c7807fc3d15a08a9e55b41027ad0db6e78
an dong woo <akskwha@naver.com>
2023-04-14 23:22:34 +0900

    230414문제풀이(주석추가)

--- 백준/java/BOJ_15894.java
+++ 백준/java/BOJ_15894.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class BOJ_15894 {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        long n =in.nextInt();
+        long n =in.nextInt();//int 범위 초과
         System.out.println(n*4);
         in.close();
     }
