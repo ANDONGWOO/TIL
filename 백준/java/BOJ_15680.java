bc242975e90a9ea2c5c9f67ffa1279ffd9ecf6cd
an dong woo <akskwha@naver.com>
2023-04-18 23:26:47 +0900

    230418문제풀이

---
+++ 백준/java/BOJ_15680.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_15680 {
+
+    public static void main(String[] args) {
+        Scanner in =new Scanner(System.in);
+        int n = in.nextInt();
+        if(n==0){
+            System.out.println("YONSEI");
+        }else{
+            System.out.println("Leading the Way to the Future");
+        }
+        in.close();
+    }
+}
