d87c8acf85e289101c784ef92eb45032a80f5017
an dong woo <akskwha@naver.com>
2023-05-07 22:35:12 +0900

    230507문제풀이

---
+++ 백준/15894.py
@@ -0,0 +1,5 @@
+import sys
+sys.stdin = open("15894.txt", "r")
+
+s=int(input())
+print(s*4)
---
+++ 백준/java/BOJ_14581.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class BOJ_14581 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + a + "::fan:");
+        System.out.println(":fan::fan::fan:");
+        
+        in.close();
+    }
+}
