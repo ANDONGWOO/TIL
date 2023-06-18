6b98bf3f53d78617f47b2c1dbfa2874ed0c46d6d
an dong woo <akskwha@naver.com>
2023-06-18 23:39:21 +0900

    230618문제풀이

---
+++ 백준/28235.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("28235.txt", "r")
+
+
+q=input()
+
+if 'SONGDO'==q:
+    print("HIGHSCHOOL")
+if 'CODE'==q:
+    print("MASTER")
+if '2023'==q:
+    print("0611")
+if 'ALGORITHM'==q:
+    print("CONTEST")
