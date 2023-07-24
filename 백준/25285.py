f7f654ebd507897967346f8d1bdefc384533e78d
an dong woo <akskwha@naver.com>
2023-07-24 23:56:50 +0900

    230724문제풀이

---
+++ 백준/25285.py
@@ -0,0 +1,16 @@
+
+import sys
+
+sys.stdin = open("25285.txt", "r")
+
+n=int(input())
+
+for i in range(n):
+    a,b=map(float,input().split())
+    if 159.0:
+        if a<140.1:
+            print(6)
+        elif a>=140.1 and a<146.0:
+            print(5)
+        else:
+            print(4)
