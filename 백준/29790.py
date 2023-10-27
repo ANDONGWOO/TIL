477ae43f1800bd62f04c83983284097c015498d3
an dong woo <akskwha@naver.com>
2023-10-27 23:45:05 +0900

    231027문제풀이

---
+++ 백준/29790.py
@@ -0,0 +1,13 @@
+import sys
+
+sys.stdin = open("29790.txt", "r")
+
+
+a,b,c=map(int,input().split())
+
+if a>=1000 and (b>=8000 or c>=260):
+    print("Very Good")
+elif a>=1000:
+    print("Good")
+else:
+    print("Bad")
